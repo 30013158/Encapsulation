80d3790ebfccdd14d962f841de97ae19b64958ad
Rajveer Kaur <30013158@student.toiohomai.ac.nz>
2019-07-31 16:23:06 +1200

    first

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard c1 = new CreditCard();
+            c1.Name = "RAJVEER KAUR";
+            c1.Number = "1233 4343 4333";
+            c1.Pin = 2425;
+            c1.Expiry = new DateTime(2033, 10, 10);
+            Console.WriteLine($"... Credit card info...\n" + $"Name : {c1.Name}\n" + $"Expiration : {c1.Expiry}");
+            Console.ReadLine();
+        }
+    }
+    public class CreditCard
+    { //attributes
+        private string name;
+        private string number;
+        private int pin;
+        private DateTime expiry;
+        //properties - getters and setters
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Number
+        {
+           private get { return number; }
+            set { number = value; }
+        }
+        public int Pin
+        {
+           private get { return pin; }
+            set { pin = value; }
+        }
+        public DateTime Expiry
+        {
+            get { return expiry; }
+            set { expiry = value; }
+        }
+        //Constructors
+        public CreditCard( string _name , string number, int pin, DateTime expiry)
+        { Name = _name;
+            Number = number;
+            Pin = pin;
+            Expiry = expiry;
+        }
+         public CreditCard() { }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] peeps = new Employee[5];
+            for(int i = 0; i < 5; i++)
+            {
+                Employee e1 = new Employee();
+                Console.Write("Name: ");
+                e1.Name = Console.ReadLine();
+                Console.Write("Age: ");
+                e1.Age = int.Parse(Console.ReadLine());
+                peeps[i] = e1;
+            }
+
+        }
+        static void Display (Employee[] arr)
+        {
+            foreach(Employee e in arr)
+                Console.WriteLine(e.Name);
+        }
+    }
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
